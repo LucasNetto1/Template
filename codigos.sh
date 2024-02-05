96eefb33170b38937f7f658d2761cc56e0c191d0
Lucas Netto <159018669+LucasNetto1@users.noreply.github.com>
2024-02-05 10:07:50 -0300

    Update codigos.sh

--- codigos.sh
+++ codigos.sh
@@ -1,6 +1,8 @@
 bin/bash
+echo 'Começaremos os downloads'
 sudo apt update;
 sudo apt upgrade;
+echo 'Agora podemos instalar o apache'
 sudo apt install mysql-server;
 sudo apt install php;
 sudo apt install phpmyadmin;
